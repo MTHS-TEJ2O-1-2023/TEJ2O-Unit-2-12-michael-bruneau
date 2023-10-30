596704319676a4c0141ba996b1e1aa71ed136cf5
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-30 12:56:54 -0400

    done

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael B
+ * Created on: Oct 2023
+ * This program lights up neopixels based on distance from objects
 */
 
 // variables
