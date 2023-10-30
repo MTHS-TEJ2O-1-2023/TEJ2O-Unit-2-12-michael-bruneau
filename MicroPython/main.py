e02db77f2bf914a82e670a2d8f665827bacf76ab
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-30 00:51:46 +0000

    setup

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,20 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Michael B
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
 
+# variables
+np = neopixel.NeoPixel(pin16, 5)
 
-display.scroll("Hello, World!")
+# setup
+display.clear
+display.shwo(Image.HAPPY)
+np[0] = (0, 0, 0)
+np[1] = (0, 0, 0)
+np[2] = (0, 0, 0)
+np[3] = (0, 0, 0)
+np.show()
